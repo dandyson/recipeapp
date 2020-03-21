a5e39ce44bdef872670e63fec27d8b8b1a45d570
terraformmedia <31185372+terraformmedia@users.noreply.github.com>
2020-03-21 21:53:27 +0100

    style changes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import '../node_modules/bootstrap/dist/css/bootstrap.css';
-import App from './App.js';
+import React from "react";
+import ReactDOM from "react-dom";
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import App from "./App.js";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
+ReactDOM.render(<App />, document.getElementById("root"));
