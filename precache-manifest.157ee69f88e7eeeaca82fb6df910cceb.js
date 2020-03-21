aca6792e457788da26f269c35a429c279f26dbaf
terraformmedia <31185372+terraformmedia@users.noreply.github.com>
2020-03-21 22:25:17 +0100

    Updates

---
+++ precache-manifest.157ee69f88e7eeeaca82fb6df910cceb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c72f2dea521bdad0419442c9f278207",
+    "url": "/recipeapp/index.html"
+  },
+  {
+    "revision": "f4f23a235c31cc83b2d0",
+    "url": "/recipeapp/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "56989c725bfbb355c491",
+    "url": "/recipeapp/static/css/main.5978eef0.chunk.css"
+  },
+  {
+    "revision": "f4f23a235c31cc83b2d0",
+    "url": "/recipeapp/static/js/2.2bbfdd95.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/recipeapp/static/js/2.2bbfdd95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56989c725bfbb355c491",
+    "url": "/recipeapp/static/js/main.53e9f9c0.chunk.js"
+  },
+  {
+    "revision": "3e5ff228aba764c51c4b",
+    "url": "/recipeapp/static/js/runtime-main.20ef2304.js"
+  }
+]);
